8d6e06419aa1888fd429bcc6c3e3549a5e7fca5c
RealAbdurRehman <claudethebitplayer@gmail.com>
2026-08-20 16:08:47 +0500

    Update lightning

--- src/core/Scene.ts
+++ src/core/Scene.ts
@@ -13,15 +13,15 @@ export class Scene {
   private init(): void {
     this.instance.add(this.background.instance);
 
-    const ambientLight = new THREE.AmbientLight(0xffffff, 0.15);
+    const ambientLight = new THREE.AmbientLight(0x101525, 0.15);
     this.instance.add(ambientLight);
 
-    const keyLight = new THREE.DirectionalLight(0xffffff, 2.5);
-    keyLight.position.set(5, 5, 5);
+    const keyLight = new THREE.DirectionalLight(0x6f8cff, 1.8);
+    keyLight.position.set(4, 6, 5);
     this.instance.add(keyLight);
 
-    const fillLight = new THREE.DirectionalLight(0xffffff, 1);
-    fillLight.position.set(-5, 2, -5);
-    this.instance.add(fillLight);
+    const rimLight = new THREE.DirectionalLight(0x9b6cff, 2.0);
+    rimLight.position.set(-5, 2, -6);
+    this.instance.add(rimLight);
   }
 }
